09b598d8683e1607e0b2cdd5946d3742761b1983
ayeshaamreen-25 <cmeayeshaamreen@gmail.com>
2025-09-08 10:20:56 +0530

    added saving logic in recommendapi file

--- APIfiles/SymcheckAPI.py
+++ APIfiles/SymcheckAPI.py
@@ -176,5 +176,3 @@ if __name__ == "__main__":
    print("🚀 Starting FastAPI on http://127.0.0.1:8001") 
    uvicorn.run(app, host="127.0.0.1", port=8001, reload=True)
    print("❌ Uvicorn exited")
-
-
